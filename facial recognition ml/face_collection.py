8c7804b02b1e0432f67898c98c294ec1d30d70a4
Tristan Sim <93330584+ssim3@users.noreply.github.com>
2023-08-26 16:22:57 +0800

    Added Jeremys data, created trainer

--- facial recognition ml/face_collection.py
+++ facial recognition ml/face_collection.py
@@ -1,16 +1,31 @@
 import cv2
 import os
+import uuid
+import time
 
-cap = cv2.VideoCapture(0)
-print('Collecting images for {}'.format(label))
-time.sleep(5)
-for imgnum in range(number_imgs):
-    print('Collecting image {}'.format(imgnum))
-    ret, frame = cap.read()
-    imgname = os.path.join(IMAGES_PATH,label,label+'.'+'{}.jpg'.format(str(uuid.uuid1())))
-    cv2.imwrite(imgname, frame)
-    cv2.imshow('frame', frame)
+def collect_data(name):
+    IMAGES_PATH = "C:/Users/Tristan Sim/OneDrive/Documents/GitHub/sutdwasdoftime/facial recognition ml/image_data/" + name
+
+    if not os.path.exists(IMAGES_PATH):
+        os.makedirs(IMAGES_PATH)
+
+    cap = cv2.VideoCapture(0)
+
+    print('Collecting images for {}'.format(name))
     time.sleep(2)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    for imgnum in range(50):   
+        print('Collecting image {}'.format(imgnum))
+        ret, frame = cap.read()
+
+        imgname = os.path.join(IMAGES_PATH,name+'.'+'{}.jpg'.format(str(uuid.uuid1())))
+
+        cv2.imwrite(imgname, frame)
+        cv2.imshow('frame', frame)
+
+        time.sleep(1)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+collect_data("Jeremy")
--- facial recognition ml/face_trainer.py
+++ facial recognition ml/face_trainer.py
@@ -4,64 +4,46 @@ import numpy as np
 from PIL import Image
 import pickle
 
-cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+cascade = cv2.CascadeClassifier("C:/Users/Tristan Sim/OneDrive/Documents/GitHub/sutdwasdoftime/facial recognition ml/haarcascade_frontalface_default.xml")
 
 recognise = cv2.face.LBPHFaceRecognizer_create()
 
-def getdata():
+def traindata(name):
 
+    face_dir = "C:/Users/Tristan Sim/OneDrive/Documents/GitHub/sutdwasdoftime/facial recognition ml/image_data/" + name
     current_id = 0
     label_id = {} #dictionanary
     face_train = [] # list
     face_label = [] # list
-    
-    # Finding the path of the base directory i.e path were this file is placed
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-    # We have created "image_data" folder that contains the data so basically
-    # we are appending its path to the base path
-    my_face_dir = os.path.join(BASE_DIR,'image_data')
 
     # Finding all the folders and files inside the "image_data" folder
-    for root, dirs, files in os.walk(my_face_dir):
-        for file in files:
+    for file in os.listdir(face_dir):
+        filename = os.fsdecode(file)
 
-            # Checking if the file has extention ".png" or ".jpg"
-            if file.endswith("png") or file.endswith("jpg"):
+        path = os.path.join(face_dir, file)
+        # providing label ID as 1 or 2 and so on for different persons
+        if not name in label_id:
+            label_id[name] = current_id
+            current_id += 1
+        ID = label_id[name]
+        # converting the image into gray scale image
+        # you can also use cv2 library for this action
+        pil_image = Image.open(path).convert("L")
+        # converting the image data into numpy array
+        image_array = np.array(pil_image, "uint8")
 
-                # Adding the path of the file with the base path
-                # so you basically have the path of the image 
-                path = os.path.join(root, file)
+        # identifying the faces
+        face = cascade.detectMultiScale(image_array)
+        # finding the Region of Interest and appending the data
+        for x,y,w,h in face:
+            img = image_array[y:y+h, x:x+w]
+        #image_array = cv2.rectangle(image_array,(x,y),(x+w,y+h),(255,255,255),3)
+            cv2.imshow("Test",img)
+            cv2.waitKey(1)
+            face_train.append(img)
+            face_label.append(ID)
 
-                # Taking the name of the folder as label i.e his/her name
-                label = os.path.basename(root).lower()
-
-                # providing label ID as 1 or 2 and so on for different persons
-                if not label in label_id:
-                    label_id[label] = current_id
-                    current_id += 1
-                ID = label_id[label]
-
-                # converting the image into gray scale image
-                # you can also use cv2 library for this action
-                pil_image = Image.open(path).convert("L")
-
-                # converting the image data into numpy array
-                image_array = np.array(pil_image, "uint8")
-        
-                # identifying the faces
-                face = cascade.detectMultiScale(image_array)
-
-                # finding the Region of Interest and appending the data
-                for x,y,w,h in face:
-                    img = image_array[y:y+h, x:x+w]
-                #image_array = cv2.rectangle(image_array,(x,y),(x+w,y+h),(255,255,255),3)
-                    cv2.imshow("Test",img)
-                    cv2.waitKey(1)
-                    face_train.append(img)
-                    face_label.append(ID)
-
-    # string the labels data into a file
+  # string the labels data into a file
     with open("labels.pickle", 'wb') as f:
         pickle.dump(label_id, f)
    
@@ -69,6 +51,8 @@ def getdata():
     return face_train,face_label
 
 # creating ".yml" file
-face,ids = getdata()
+name = "Jeremy"
+face,ids = traindata(name)
+
 recognise.train(face, np.array(ids))
-recognise.save("trainner.yml")  
+recognise.save(name + "_trainer.yml")  
